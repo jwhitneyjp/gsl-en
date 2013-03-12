42abd5238fac95d5833f3aa46b119a0b4369452b
Frank Bennett <biercenator@gmail.com>
2013-03-12 12:39:31 +0900

    Fix access perms of top-level directory on upload

--- uploadme.sh
+++ uploadme.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find ./release/docroot -type d -exec chmod uog+x {} \;
 find ./release/docroot -exec chmod uog+r {} \;
+rm -f ./release/docroot/en
 rsync -av ./release/docroot/ en@law.nagoya-u.ac.jp:/var/www/html/en

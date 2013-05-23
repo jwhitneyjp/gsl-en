ea13c43a8bb75d17b63d9298fbe60e81d52cd808
Frank Bennett <biercenator@gmail.com>
2013-05-23 16:18:41 +0900

    Make staffdump a little friendlier

--- staffinfodump.sh
+++ staffinfodump.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-src/python/bin/staffblast.py -d -f status,family_name,given_name,proper_name,birthdate,field,affiliation,phone,office,office_hours,photo_web_ok,email,email_disclose_ok,website,degrees,career_history,research_interests,memberships,visitorships,publications,recommended_readings,preparation_suggestions -r $1
+echo Usage: scriptname.sh --help
+echo Usage: scriptname.sh -l
+echo "Usage: scriptname.sh <comma_delimited_list_of_names>"
 
+if [ "" == "$1" ]; then
+  ./src/python/bin/staffblast.py --help
+elif [ "-l" == "$1" ]; then
+  ./src/python/bin/staffblast.py -l
+else
+  echo $1
+  ./src/python/bin/staffblast.py -m hello.txt -f status,family_name,given_name,proper_name,birthdate,field,affiliation,phone,office,office_hours,photo_web_ok,email,email_disclose_ok,website,degrees,career_history,research_interests,memberships,visitorships,publications,recommended_readings,preparation_suggestions \
+    -r $1
+fi
